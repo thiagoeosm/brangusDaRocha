95384d53bb8912a630976dd4b88c71bf7bbac7fc
THIAGO EGGRES <thiago.eggres@idwall.co>
2023-08-03 19:31:14 -0300

    wip: form in progress

--- vue-project/src/views/FormView.vue
+++ vue-project/src/views/FormView.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="about">
       <h1>This is a FORM</h1>
+      <form>
+        <label>Nome</label>
+        <input type="text" id="name-box">
+      </form>
     </div>
   </template>
+
+  <script>
+  export default {
+    data() {
+        return {
+            nomeQueEuEscrevi: 'thiago'
+        }
+    }
+  }
+
+  </script>
   
   <style>
   @media (min-width: 1024px) {
@@ -11,5 +26,15 @@
       display: flex;
       align-items: center;
     }
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    max-width: 200px;
+  }
+
+  .name-box{
+    max-width: 10px;
   }
   </style>
